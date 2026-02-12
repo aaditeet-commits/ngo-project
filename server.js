e81037d62f619ce5476ce7c9d58d03195948a7ee
Aaditee Thorat <aaditeethorat@gmail.com>
2026-02-12 13:21:59 +0530

    fix mysql issue

--- server.js
+++ server.js
@@ -17,14 +17,6 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT
 });
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
-
 db.connect((err) => {
     if (err) {
         console.error('Database connection failed:', err);
@@ -85,6 +77,7 @@ db.connect((err) => {
         console.log('Tables created or already exist');
     }
 });
+
 
 
 // Middleware
